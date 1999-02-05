c8ae6f39d468f84c56aadd5d65f3436b7722ab0d
dk <dk>
1999-02-05 18:46:48 +0000

    - AbstractMenu.c fix echo

--- Menu.c
+++ Menu.c
@@ -10,30 +10,16 @@
 
 
 void
-Menu_done( Handle self)
-{
-   ((( PWindow) var owner)-> self)-> set_menu( var owner, nilHandle);
-   inherited done( self);
-}
-
-void
 Menu_update_sys_handle( Handle self, HV * profile)
 {
    Handle xOwner = pexist( owner) ? pget_H( owner) : var owner;
-   if ( var owner && ( xOwner != var owner)) ((( PWindow) var owner)-> self)-> set_menu( var owner, nilHandle);
+   if ( var owner && ( xOwner != var owner))
+      ((( PWindow) var owner)-> self)-> set_menu( var owner, nilHandle);
    var anchored = true;
-   if (!(
-       pexist( owner) ||
-       pexist( items)
-    )) return;
-   if ( pexist( items)) {
-      my dispose_menu( self, var tree);
-      var tree = my new_menu( self, pget_sv( items), 0);
-   }
+   if ( !pexist( owner)) return;
    if ( !apc_menu_create( self, xOwner))
       croak("RTC0060: Cannot create menu");
    pdelete( owner);
-   pdelete( items);
 }
 
 void
