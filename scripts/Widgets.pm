72ec43891b04d8acdaffdc348e4d431141f444d1
dk <dk>
1999-02-11 11:17:39 +0000

    - object ImagePanel added - on the way to alloy with Panel.

--- scripts/Widgets.pm
+++ scripts/Widgets.pm
@@ -2,6 +2,7 @@ package Widgets;
 
 # contains:
 #   Panel
+#   ImagePanel
 #   Scroller
 
 use Const;
@@ -66,6 +67,138 @@ sub on_paint
 
 sub indent {($#_)?($_[0]->{indent} = $_[1],$_[0]-> repaint):return $_[0]->{indent};}
 sub raise  {($#_)?($_[0]->{raise } = $_[1],$_[0]-> repaint):return $_[0]->{raise };}
+
+package ImagePanel;
+use vars qw(@ISA);
+@ISA = qw( Widget);
+
+sub profile_default
+{
+   my $def = $_[0]-> SUPER::profile_default;
+   my %prf = (
+      raise        => 1,
+      borderWidth  => 1,
+      image        => undef,
+      imageFile    => undef,
+      zoom         => 1,
+   );
+   @$def{keys %prf} = values %prf;
+   return $def;
+}
+
+sub profile_check_in
+{
+   my ( $self, $p, $default) = @_;
+   $self-> SUPER::profile_check_in( $p, $default);
+   if ( defined $p->{imageFile} && !defined $p->{image})
+   {
+      $p->{image} = Image-> create;
+      delete $p->{image} unless $p->{image}-> load($p->{imageFile});
+   }
+}
+
+sub init
+{
+   my $self = shift;
+   for ( qw( image ImageFile))
+      { $self->{$_} = undef; }
+   for ( qw( zoom raise borderWidth iw ih)) { $self->{$_} = 1; }
+   my %profile = $self-> SUPER::init(@_);
+   $self-> { imageFile}  = $profile{ imageFile};
+   for ( qw( image zoom borderWidth raise)) {
+      $self->$_($profile{$_});
+   }
+   return %profile;
+}
+
+sub on_paint
+{
+   my ( $self, $canvas) = @_;
+   my @size   = $canvas-> size;
+   my $clr    = $self-> backColor;
+   my $bw     = $self-> {borderWidth};
+   my @c3d    = ( $self-> light3DColor, $self-> dark3DColor);
+   @c3d = reverse @c3d unless $self->{raise};
+   unless ( defined $self->{image}) {
+      $canvas-> rect3d( 0, 0, $size[0]-1, $size[1]-1, $bw, @c3d, $clr);
+      return;
+   }
+   $canvas-> rect3d( 0, 0, $size[0]-1, $size[1]-1, $bw, @c3d) if $bw > 0;
+   my ( $x, $y) = ( $bw, $bw);
+   my ( $dx, $dy ) = ( $self-> {iw}, $self->{ih});
+   if ( $bw > 0) {
+      $canvas-> clipRect( $bw, $bw, $size[0] - $bw - 1, $size[1] - $bw - 1);
+      $size[0] -= $bw;
+      $size[1] -= $bw;
+   }
+   while ( $x < $size[0]) {
+      $y = $bw;
+      while ( $y < $size[1]) {
+         $self-> stretch_image( $x, $y, $dx, $dy, $self->{image});
+         $y += $dy;
+      }
+      $x += $dx;
+   }
+}
+
+sub set_border_width
+{
+   my ( $self, $bw) = @_;
+   $bw = 0 if $bw < 0;
+   $bw = int( $bw);
+   return if $bw == $self->{borderWidth};
+   $self->{borderWidth} = $bw;
+   $self-> repaint;
+}
+
+sub set_raise
+{
+   my ( $self, $r) = @_;
+   return if $r == $self->{raise};
+   $self->{raise} = $r;
+   $self-> repaint;
+}
+
+sub set_image_file
+{
+   my ($self,$file,$img) = @_;
+   $img = Image-> create;
+   return unless $img-> load($file);
+   $self->{imageFile} = $file;
+   $self->image($img);
+}
+
+sub set_image
+{
+   my ( $self, $img) = @_;
+   $self->{image} = $img;
+   return unless defined $img;
+   ( $self->{iw}, $self->{ih}) = ($img-> width * $self->{zoom}, $img-> height * $self->{zoom});
+   $self-> {image} = undef if $self->{ih} == 0 || $self->{iw} == 0;
+   $self-> repaint;
+}
+
+
+sub set_zoom
+{
+   my ( $self, $zoom) = @_;
+   $zoom = int( $zoom);
+   $zoom = 1 if $zoom < 1;
+   $zoom = 10 if $zoom > 10;
+   return if $zoom == $self->{raise};
+   $self->{zoom} = $zoom;
+   return unless defined $self->{image};
+   my $img = $self->{img};
+   ( $self->{iw}, $self->{ih}) = ( $img-> width * $self->{zoom}, $img-> height * $self->{zoom});
+   $self-> repaint;
+}
+
+sub image        {($#_)?$_[0]->set_image($_[1]):return $_[0]->{image} }
+sub imageFile    {($#_)?$_[0]->set_image_file($_[1]):return $_[0]->{imageFile}}
+sub zoom         {($#_)?$_[0]->set_zoom($_[1]):return $_[0]->{zoom}}
+sub borderWidth  {($#_)?$_[0]->set_border_width($_[1]):return $_[0]->{borderWidth}}
+sub raise        {($#_)?$_[0]->set_raise($_[1]):return $_[0]->{raise}}
+
 
 package Scroller;
 use vars qw(@ISA);
