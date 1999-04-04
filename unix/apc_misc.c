394fec9c05c8a2ca93ea01ec3285b6ce7f680983
tobez <tobez>
1999-04-04 19:46:19 +0000

    - debug print -> DOLBUG();

--- unix/apc_misc.c
+++ unix/apc_misc.c
@@ -57,46 +57,46 @@ apc_component_destroy( Handle self)
 void
 apc_cursor_set_pos( Handle self, int x, int y)
 {
-fprintf( stderr, "apc_cursor_set_pos()\n");
+   DOLBUG( "apc_cursor_set_pos()\n");
 }
 
 void
 apc_cursor_set_size( Handle self, int x, int y)
 {
-fprintf( stderr, "apc_cursor_set_size()\n");
+   DOLBUG( "apc_cursor_set_size()\n");
 }
 
 void
 apc_cursor_set_visible( Handle self, Bool visible)
 {
-fprintf( stderr, "apc_cursor_set_visible()\n");
+   DOLBUG( "apc_cursor_set_visible()\n");
 }
 
 Point
 apc_cursor_get_pos( Handle self)
 {
-fprintf( stderr, "apc_cursor_get_pos()\n");
+   DOLBUG( "apc_cursor_get_pos()\n");
    return (Point){0,0};
 }
 
 Point
 apc_cursor_get_size( Handle self)
 {
-fprintf( stderr, "apc_cursor_get_size()\n");
+   DOLBUG( "apc_cursor_get_size()\n");
    return (Point){0,0};
 }
 
 Bool
 apc_cursor_get_visible( Handle self)
 {
-fprintf( stderr, "apc_cursor_get_visible()\n");
+   DOLBUG( "apc_cursor_get_visible()\n");
    return false;
 }
 
 Point
 apc_pointer_get_hot_spot( Handle self)
 {
-fprintf( stderr, "apc_pointer_get_hot_spot()\n");
+   DOLBUG( "apc_pointer_get_hot_spot()\n");
    return (Point){0,0};
 }
 
@@ -120,67 +120,67 @@ apc_pointer_get_pos( Handle self)
 int
 apc_pointer_get_shape( Handle self)
 {
-fprintf( stderr, "apc_pointer_get_shape()\n");
+   DOLBUG( "apc_pointer_get_shape()\n");
    return 0;
 }
 
 Point
 apc_pointer_get_size( Handle self)
 {
-fprintf( stderr, "apc_pointer_get_size()\n");
+   DOLBUG( "apc_pointer_get_size()\n");
    return (Point){0,0};
 }
 
 Bool
 apc_pointer_get_bitmap( Handle self, Handle icon)
 {
-fprintf( stderr, "apc_pointer_get_bitmap()\n");
+   DOLBUG( "apc_pointer_get_bitmap()\n");
    return false;
 }
 
 Bool
 apc_pointer_get_visible( Handle self)
 {
-fprintf( stderr, "apc_pointer_get_visible()\n");
+   DOLBUG( "apc_pointer_get_visible()\n");
    return false;
 }
 
 void
 apc_pointer_set_pos( Handle self, int x, int y)
 {
-fprintf( stderr, "apc_pointer_set_pos()\n");
+   DOLBUG( "apc_pointer_set_pos()\n");
 }
 
 void
 apc_pointer_set_shape( Handle self, int sysPtrId)
 {
-fprintf( stderr, "apc_pointer_set_shape()\n");
+   DOLBUG( "apc_pointer_set_shape()\n");
 }
 
 Bool
 apc_pointer_set_user( Handle self, Handle icon, Point hotSpot)
 {
-fprintf( stderr, "apc_pointer_set_user()\n");
+   DOLBUG( "apc_pointer_set_user()\n");
    return false;
 }
 
 void
 apc_pointer_set_visible( Handle self, Bool visible)
 {
-fprintf( stderr, "apc_pointer_set_visible()\n");
+   DOLBUG( "apc_pointer_set_visible()\n");
 }
 
 int
 apc_pointer_get_state( Handle self)
 {
-fprintf( stderr, "apc_pointer_get_state()\n");
+   DOLBUG( "apc_pointer_get_state()\n");
    return 0;
 }
 
 int
 apc_kbd_get_state( Handle self)
 {
-fprintf( stderr, "apc_kbd_get_state()\n");
+   DOLBUG( "apc_kbd_get_state()\n");
    return 0;
 }
 
@@ -190,67 +190,67 @@ fprintf( stderr, "apc_kbd_get_state()\n");
 Bool
 apc_clipboard_create( void)
 {
-fprintf( stderr, "apc_clipboard_create()\n");
+   DOLBUG( "apc_clipboard_create()\n");
    return true;
 }
 
 void
 apc_clipboard_destroy( void)
 {
-fprintf( stderr, "apc_clipboard_destroy()\n");
+   DOLBUG( "apc_clipboard_destroy()\n");
 }
 
 Bool
 apc_clipboard_open( void)
 {
-fprintf( stderr, "apc_clipboard_open()\n");
+   DOLBUG( "apc_clipboard_open()\n");
    return false;
 }
 
 void
 apc_clipboard_close( void)
 {
-fprintf( stderr, "apc_clipboard_close()\n");
+   DOLBUG( "apc_clipboard_close()\n");
 }
 
 void
 apc_clipboard_clear( void)
 {
-fprintf( stderr, "apc_clipboard_clear()\n");
+   DOLBUG( "apc_clipboard_clear()\n");
 }
 
 Bool
 apc_clipboard_has_format( long id)
 {
-fprintf( stderr, "apc_clipboard_has_format()\n");
+   DOLBUG( "apc_clipboard_has_format()\n");
    return false;
 }
 
 void *
 apc_clipboard_get_data( long id, int *length)
 {
-fprintf( stderr, "apc_clipboard_get_data()\n");
+   DOLBUG( "apc_clipboard_get_data()\n");
    return nil;
 }
 
 Bool
 apc_clipboard_set_data( long id, void * data, int length)
 {
-fprintf( stderr, "apc_clipboard_set_data()\n");
+   DOLBUG( "apc_clipboard_set_data()\n");
    return false;
 }
 
 long
 apc_clipboard_register_format( char * format)
 {
-fprintf( stderr, "apc_clipboard_register_format()\n");
+   DOLBUG( "apc_clipboard_register_format()\n");
    return 0;
 }
 
 void
 apc_clipboard_deregister_format( long id)
 {
-fprintf( stderr, "apc_clipboard_deregister_format()\n");
+   DOLBUG( "apc_clipboard_deregister_format()\n");
 }
 
 /* Timer */
@@ -367,20 +367,20 @@ apc_timer_stop( Handle self)
 Bool
 apc_help_open_topic( Handle self, long command)
 {
-fprintf( stderr, "apc_help_open_topic()\n");
+   DOLBUG( "apc_help_open_topic()\n");
    return false;
 }
 
 void
 apc_help_close( Handle self)
 {
-fprintf( stderr, "apc_help_close()\n");
+   DOLBUG( "apc_help_close()\n");
 }
 
 void
 apc_help_set_file( Handle self, char * helpFile)
 {
-fprintf( stderr, "apc_help_set_file()\n");
+   DOLBUG( "apc_help_set_file()\n");
 }
 
 
@@ -389,13 +389,13 @@ fprintf( stderr, "apc_help_set_file()\n");
 void
 apc_message( Handle self, PEvent ev, Bool post)
 {
-fprintf( stderr, "apc_message()\n");
+   DOLBUG( "apc_message()\n");
 }
 
 void
 apc_show_message( char * message)
 {
-fprintf( stderr, "apc_show_message()\n");
+   DOLBUG( "apc_show_message()\n");
 }
 
 /* system metrics */
@@ -409,14 +409,14 @@ apc_sys_get_insert_mode( void)
 PFont
 apc_sys_get_msg_font( PFont copyTo)
 {
-fprintf( stderr, "apc_sys_get_msg_font()\n");
+   DOLBUG( "apc_sys_get_msg_font()\n");
    return nil;
 }
 
 PFont
 apc_sys_get_caption_font( PFont copyTo)
 {
-fprintf( stderr, "apc_sys_get_caption_font()\n");
+   DOLBUG( "apc_sys_get_caption_font()\n");
    return nil;
 }
 
@@ -475,39 +475,39 @@ apc_beep( int style)
 void
 apc_beep_tone( int freq, int duration)
 {
-fprintf( stderr, "apc_beep_tone()\n");
+   DOLBUG( "apc_beep_tone()\n");
 }
 
 char *
 apc_system_action( char * params)
 {
-fprintf( stderr, "apc_system_action()\n");
+   DOLBUG( "apc_system_action()\n");
    return nil;
 }
 
 void
 apc_query_drives_map( char *firstDrive, char *result)
 {
-fprintf( stderr, "apc_query_drives_map()\n");
+   DOLBUG( "apc_query_drives_map()\n");
 }
 
 int
 apc_query_drive_type( char *drive)
 {
-fprintf( stderr, "apc_query_drive_type()\n");
+   DOLBUG( "apc_query_drive_type()\n");
    return 0;
 }
 
 char *
 apc_get_user_name( void)
 {
-fprintf( stderr, "apc_get_user_name()\n");
+   DOLBUG( "apc_get_user_name()\n");
    return nil;
 }
 
 void *
 apc_dlopen(char *path, int mode)
 {
-fprintf( stderr, "apc_dlopen()\n");
+   DOLBUG( "apc_dlopen()\n");
    return nil;
 }
