35935efee631996ac42445b8c4c3d2eee05ba977
dk <dk>
1999-10-06 11:46:54 +0000

    *** empty log message ***

--- examples/matrix.pl
+++ examples/matrix.pl
@@ -31,24 +31,38 @@ $::application = Prima::Application->  create;
 my $smp = "The Matrix has you";
 my $maxstep = 40;
 my $ymaxstep = 60;
-my $widefactor = 0.05;
-my $shades = 3;
-my $shadesDepth = 4;
-my $xshspeed = 2;
-my $basicfsize = 10;
-my $vlines = 40;
+my $widefactor = 0.05;   # range 0.01 - 0.3
+my $digitShades = 8;     # range 1 - 20
+my $textShades = 3;      # range 1 - 20
+my $shadesDepth = 4;     # range 1 - 100
+my $xshspeed = 2;        # range 1 - 4
+my $basicfsize = 10;     # range 6 - 24
+my $vlines = 40;         # range 10 - 80
+my $textToBMRatio = 0.3; # range 0.01 - 0.9
+my $digitTicks    = 150; # range 1-...
+my $textTicks     = 30;  # range 1-...
+
 
 my $maxln = length( $smp);
 my @vlinst = map { int( rand( $ymaxstep))} 1..$vlines;
+my @vlbminst = map { int( rand( $ymaxstep))} 1..$vlines;
 my @vlsped = (( 1) x $vlines);
+my @vlbmsped = (( 1) x $vlines);
+my @vlbms  = map { int( rand( 3))} 1..$vlines;
 my @vlxcol = (( 0) x $vlines);
+my @vlbmxcol = (( 0) x $vlines);
 my $xshcnt = -1000;
 my $xshdir  = 1;
 my $xcol = 30;
 my $yextraspeed = 0;
+my $ticker = 10000000;
+my $tickerMode = 0;
+my $shades = 0;
 
-my %fsh = ();
-my %fhh = ();
+
+my %fsh  = ();
+my %fhh  = ();
+my @dbms = ();
 
 sub efont
 {
@@ -105,6 +119,9 @@ my @spal = ();
 for ( $i = 0; $i < 256; $i++) {
    push( @spal, 0, $i, 0);
 };
+my @gifs = map { Prima::Image-> load( 'matrix.gif', index => $_)} 0..2;
+@gifs = () unless $gifs[0];
+
 
 sub resetfs
 {
@@ -115,7 +132,16 @@ sub resetfs
    $self-> font-> size( $basicfsize);
    $ymaxstep = $sz[1] / $self-> font-> height + length( $smp) * 2;
    @vlxcol = map { int(rand( $sz[0] - 30)) + 15 } 1..$vlines;
+   @vlbmxcol = map { int(rand( $sz[0] - 30)) + 15 } 1..$vlines;
+   my $fw = $self-> font-> height;
+
+   @dbms = map {
+      my $x = $_-> dup;
+      $x-> size( $fw, $_-> height * $fw / 21);
+      $x-> bitmap;
+   } @gifs;
 }
+
 
 my $w = Prima::Window-> create(
    palette => [@spal],
@@ -128,23 +154,63 @@ my $w = Prima::Window-> create(
       my ( $self, $c) = @_;
       my @sz = $c-> size;
       my $cc = $self-> color;
-      $c-> color( $self-> backColor);
+
+      $ticker++;
+      my $lim = $tickerMode ? $digitTicks : $textTicks;
+      if ( $ticker > $lim) {
+         $ticker = 0;
+         $tickerMode = !$tickerMode;
+         $shades = $tickerMode ? $digitShades : $textShades;
+      }
+
+      if ( $tickerMode || ( $ticker % 2) || ( $ticker < $textTicks / 2)) {
+         $c-> color( $self-> backColor);
+      } else {
+         $c-> color( 0x00F000);
+      }
       $c-> bar( 0,0,@sz);
       $self-> {xcnt} = 1 if ++$self-> {xcnt} >= $maxstep;
+
+      my $ymans;
+      my $fh = $c-> font-> height;
+      for ( $ymans = 0; $ymans < $vlines; $ymans++) {
+         my $y = $sz[1] - $vlbminst[ $ymans] * $fh;
+         $c-> put_image( $sz[0] - $vlbmxcol[ $ymans], $y, $dbms[ $vlbms[ $ymans]]);
+         if ( ++$vlbminst[ $ymans] >= $ymaxstep) {
+            $vlbminst[ $ymans] = 1;
+            $vlbmxcol[ $ymans] = int( rand( $sz[0] - 30)) + 15;
+            $vlbmsped[ $ymans] = rand( 3) - 1;
+            $vlbmsped[ $ymans] = 0 if $vlbmsped[ $ymans] < 0;
+            $vlbmsped[ $ymans] *= 3;
+
+         }
+         $vlbminst[ $ymans] += $vlbmsped[ $ymans];
+      }
 
       for ( 0..$shades) {
          my $x = $self->{xcnt} - (( $shades - $_) * $shadesDepth);
          $x += $maxstep if $x <= 0;
          efont( $c, $x);
 
-
          #$x -= ( $shades - $_);
          #$x += $shades;
          #next if $x <= 0;
          $x = $x - (( $shades - $_) * $shadesDepth);
          ecolor( $c, $cc, $self-> backColor, $x / 30);
-         $c-> text_out( $smp,
-            ( $sz[0] - $c-> get_text_width( $smp)) / 2,
+         my $mp;
+         if ( $tickerMode) {
+            $mp = abs( $c-> font-> direction);
+            if ( $mp < 100) {
+               $mp = $mp * 10 + $mp / 10;
+            } else {
+               $mp = $mp * 100 + reverse($mp / 10);
+            }
+         } else {
+            $mp = $smp;
+         }
+
+         $c-> text_out( $mp,
+            ( $sz[0] - $c-> get_text_width( $mp)) / 2,
             ( $sz[1] - $c-> font-> height) / 2);
       }
 
@@ -155,23 +221,23 @@ my $w = Prima::Window-> create(
       );
       $c-> color( $cc);
 
-      my $ymans;
-      for ( $ymans = 0; $ymans < $vlines; $ymans++) {
+      $fh = $c-> font-> height;
+      for ( $ymans = 0; $ymans < $vlines * $textToBMRatio; $ymans++) {
          if ( ++$vlinst[ $ymans] >= $ymaxstep) {
             $vlinst[ $ymans] = 1;
             $vlxcol[ $ymans] = int( rand( $sz[0] - 30)) + 15;
-            $vlsped[ $ymans] = int(rand( 4)) - 1;
+            $vlsped[ $ymans] = rand( 3) - 1;
             $vlsped[ $ymans] = 0 if $vlsped[ $ymans] < 0;
             $vlsped[ $ymans] *= 3;
          }
-         my $fh = $c-> font-> height;
          my $y = $sz[1] - ($vlinst[ $ymans] - $maxln) * $fh;
 
          my $i;
          $vlinst[ $ymans] += $vlsped[ $ymans];
          ecolor( $c, $cc, cl::Yellow, 0.5) if $vlsped[ $ymans] > 1;
+         $y = $sz[1] - ($vlinst[ $ymans] - $maxln) * $fh;
          for ( $i = 0; $i < $maxln; $i++) {
-            $c-> text_out( substr( $smp, $i, 1), $vlxcol[ $ymans], $y);
+            $c-> text_out( substr( $smp, $i, 1), $vlxcol[ $ymans] / $textToBMRatio, $y);
             $y -= $fh;
          }
          $c-> color( $cc) if $vlsped[ $ymans] > 1;
