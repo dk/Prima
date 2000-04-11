fc19388748db3a556e847fb1aa1aace66ba31791
tobez <tobez>
2000-04-11 12:12:46 +0000

    Modernize edit.pl sample.

--- examples/edit.pl
+++ examples/edit.pl
@@ -25,40 +25,21 @@
 #
 
 
-use Prima;
-use Prima::Const;
-use Prima::Widgets;
-use Prima::InputLine;
+use Prima 'InputLine', Application => { name => 'InputLine sample' };
 
-$::application = Prima::Application-> create( name => "Generic.pm");
+my $w = Prima::Window-> create( size => [ 700, 300]);
 
-my $l;
-my $w = Prima::Window-> create(
-  size => [ 700, 300],
-  onDestroy => sub { $::application-> destroy},
-  accelItems => [
-    [a=>a=> kb::F3=>sub{$l-> repaint}],
-  ]
-
-);
-
-$l = $w-> insert( InputLine =>
-   text  => '0:::1234 5678 90ab cdef ghij klmn oprq stuv:1::1234 5678 90ab cdef ghij klmn oprq stuv:2::1234 5678 90ab cdef ghij klmn oprq stuv:3::1234 5678 90ab cdef ghij klmn oprq stuv:4::1234 5678 90ab cdef ghij klmn oprq stuv::End',
-   y_centered  => 1,
-   left   => 200,
-   width => 300,
-   #selection => [1,12],
-#  height => 40,
-#  maxLen => 15,
-   firstChar => 2,
-#  offset => 3,
-   alignment => ta::Center,
-   font   => { size => 18, name => 'Tms Rmn'},
-   growMode => gm::GrowHiX,
-   buffered => 1,
-   xonPaint => sub {},
+my $l = $w-> insert( InputLine =>
+   text        => '0:::1234 5678 90ab cdef ghij klmn oprq stuv:1::1234 5678 90ab cdef ghij klmn oprq stuv:2::1234 5678 90ab cdef ghij klmn oprq stuv:3::1234 5678 90ab cdef ghij klmn oprq stuv:4::1234 5678 90ab cdef ghij klmn oprq stuv::End',
+   centered    => 1,
+   width       => 300,
+   firstChar   => 10,
+   alignment   => ta::Center,
+   font        => { size => 18, },
+   growMode    => gm::GrowHiX,
+   buffered    => 1,
    borderWidth => 3,
-   autoSelect => 0,
+   autoSelect  => 0,
 );
 
 run Prima;
