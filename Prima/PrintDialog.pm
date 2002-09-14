60ae980971b350a3e3ba8dafc5bc423bba589647
dk <dk>
2002-09-14 22:03:53 +0000

    - pod section added

--- Prima/PrintDialog.pm
+++ Prima/PrintDialog.pm
@@ -162,3 +162,49 @@ sub execute
 }
 
 1;
+
+__DATA__
+
+=pod
+
+=head1 NAME
+
+Prima::PrintDialog - standard printer setup dialog
+
+=head1 DESCRIPTION
+
+Provides a stantard dialog that allows the user to select a printer
+and its options. The toolkit does not provide the in-depth management
+of the printer options; this can only be accessed by executing a printer-specific
+setup window, called by C<Prima::Printer::setup_dialog>. The class invokes
+this method when the user presses 'Properties' button. Otherwise, the class
+provides only selection of a printer from the printer list.
+
+When the dialog finished successfully, the selected printer is set as the current
+by writing to C<Prima::Printer::printer> property. This technique allows direct use of
+the user-selected printer and its properties without prior knowledge of the
+selection process.
+
+=head1 SYNOPSIS
+
+   use Prima::PrintDialog;
+   
+   $dlg = Prima::PrintSetupDialog-> create;
+   if ( $dlg-> execute) {
+      my $p = $::application-> get_printer;
+      if ( $p-> begin_doc ) {
+         $p-> text_out( 'Hello world', 10, 10);
+         $p-> end_doc;
+      }
+   }
+   $dlg-> destroy;
+
+=head1 AUTHOR
+
+Dmitry Karasik, E<lt>dmitry@karasik.eu.orgE<gt>.
+
+=head1 SEE ALSO
+
+L<Prima>, L<Prima::Window>, L<Prima::Printer>.
+
+=cut
