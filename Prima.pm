f22d0404ccc4fa07814656ac20f4433584e7ad7e
dk <dk>
2002-11-08 13:05:50 +0000

    - Version 1.08

--- Prima.pm
+++ Prima.pm
@@ -34,7 +34,7 @@ require DynaLoader;
 use vars qw($VERSION @ISA $__import);
 @ISA = qw(DynaLoader);
 sub dl_load_flags { 0x00 }
-$VERSION = '1.07';
+$VERSION = '1.08';
 bootstrap Prima $VERSION;
 unless ( UNIVERSAL::can('Prima', 'init')) {
    $::application = 0;
