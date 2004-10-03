2cea81b345635e7fbd1172ba32bc4f72dbea89c1
dk <dk>
2004-10-03 13:54:30 +0000

    - Release v1.16

--- Prima.pm
+++ Prima.pm
@@ -34,7 +34,7 @@ require DynaLoader;
 use vars qw($VERSION @ISA $__import);
 @ISA = qw(DynaLoader);
 sub dl_load_flags { 0x00 }
-$VERSION = '1.15';
+$VERSION = '1.16';
 bootstrap Prima $VERSION;
 unless ( UNIVERSAL::can('Prima', 'init')) {
    $::application = 0;
