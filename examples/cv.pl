41bf05a2d79db1be9fa20d90614a5fd044017899
dk <dk>
2005-10-17 14:22:42 +0000

    - display a banner with an invitation to drag colors from the color wheel

--- examples/cv.pl
+++ examples/cv.pl
@@ -39,6 +39,7 @@ compbinations of Shift,Alt,and Control.
 
 =cut
 
+use strict;
 use Prima 'StdDlg', Application => { name => 'CV' };
 
 my $p = Prima::ColorDialog-> create(
@@ -46,5 +47,29 @@ my $p = Prima::ColorDialog-> create(
 	visible => 1,
 	quality => 1,
 );
+
+my $banner = $p-> {wheel}-> insert( Label => 
+	text => <<MSG,
+Drag colors from the color wheel by left mouse button together with combinations of Alt, Shift, and Control
+MSG
+	autoHeight => 1,
+	wordWrap   => 1,
+	transparent => 1,
+	alignment => ta::Center,
+	left  => $p-> {wheel}-> width * 0.125,
+	top => 0,
+	width => $p-> {wheel}-> width * 0.75,
+);
+
+$p-> insert( Timer => 
+	timeout => 100,
+	onTick  => sub {
+		if ( $banner-> bottom > $p->{wheel}-> height) {
+			$_[0]-> destroy;
+		} else {
+			$banner-> bottom( $banner-> bottom + 2);
+		}
+	},
+)-> start;
 
 $p-> execute;
