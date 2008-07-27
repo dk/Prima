0632243befa453aca0cf62ebb6a78cec3e09c065
dk <dk>
2008-07-27 14:39:52 +0000

    - fix failed ::dup() for images with type=imbpp4|imGrayScale

--- img/imgtype.c
+++ img/imgtype.c
@@ -540,7 +540,7 @@ ic_type_convert( Handle self, Byte * dstData, PRGBColor dstPal, int dstType, int
 
 static int imTypes[] = {
    imbpp1, imbpp1|imGrayScale, 
-   imbpp4, 
+   imbpp4, imbpp4|imGrayScale,
    imbpp8, imbpp8|imGrayScale,
    imRGB, 
    imShort, imLong, imFloat, imDouble,
