dcb5eefd12c444e2cdd66ef1f31f6eb7b849feca
dk <dk>
2008-07-27 14:38:26 +0000

    - save/restore some default HDC values that are different on XP, and   that were implicitly trusted

--- win32/apc.c
+++ win32/apc.c
@@ -1718,6 +1718,7 @@ apc_widget_begin_paint( Handle self, Bool insideOnPaint)
 
    if ( useRPDraw) {
       HDC dc;
+      HGDIOBJ o_pen, o_brush, o_font, o_pal, o_extpen;
       Handle owner = var owner;
       Point tr = dsys(owner)transform2;
       Point ed = apc_widget_get_pos( self);
@@ -1730,8 +1731,22 @@ apc_widget_begin_paint( Handle self, Bool insideOnPaint)
       dsys(owner) transform2. x += ed. x;
       dsys(owner) transform2. y += so. y - sz. y - ed. y;
       apc_gp_set_transform( owner, 0, 0);
+      apc_gp_set_text_out_baseline( owner, dsys(owner) options. aptTextOutBaseline);
+
+      SelectObject( sys ps, o_pen    = GetCurrentObject( dc, OBJ_PEN));
+      SelectObject( sys ps, o_brush  = GetCurrentObject( dc, OBJ_BRUSH));
+      SelectObject( sys ps, o_font   = GetCurrentObject( dc, OBJ_FONT));
+      SelectObject( sys ps, o_pal    = GetCurrentObject( dc, OBJ_PAL));
+      SelectObject( sys ps, o_extpen = GetCurrentObject( dc, OBJ_EXTPEN));
 
       CWidget( owner)-> notify( owner, "sH", "Paint", owner);
+
+      SelectObject( sys ps, o_pen);
+      SelectObject( sys ps, o_brush);
+      SelectObject( sys ps, o_font);
+      SelectObject( sys ps, o_pal);
+      SelectObject( sys ps, o_extpen);
+
       dsys(owner)transform2 = tr;
       apc_gp_set_transform( owner, 0, 0);
       dsys( owner) ps = dc;
