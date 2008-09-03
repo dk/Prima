4971a4f5677e8204f811d11494a7456a1477f174
dk <dk>
2008-09-03 11:29:13 +0000

    - add more docs on image use without x11

--- Prima/noX11.pm
+++ Prima/noX11.pm
@@ -42,3 +42,79 @@ sub XOpenDisplay
 }
 
 1;
+
+__DATA__
+
+=for rcs $Id$
+
+=head1 NAME
+
+Prima::noX11 - Use Prima without X11
+
+=head1 SYNOPSIS
+
+   use Prima::noX11;
+   use Prima;
+
+   my $error = Prima::XOpenDisplay();
+   if ( defined $error) {
+        print "not connected to display: $error\n";
+   } else {
+   	print "connected to display\n";
+   }
+
+=head1 DESCRIPTION
+
+Prima will by default connect to X11 server on unix. To use
+Prima functionality in modules or programs where this default
+behavior is undesired, please follow the guidelines below.
+
+=head2 No connection
+
+In the beginning of a script or a module that is never intended to connect to
+X11 display, add this:
+
+  use Prima::noX11;
+  use Prima;
+
+It will be possible to connect to X11 server later on manually.
+
+=head2 Manual connect to X11
+
+If connection to X11 is optional, use this code after C<use Prima::noX11>
+was invoked:
+
+   my $error = Prima::XOpenDisplay();
+   if ( defined $error) {
+        print "not connected to display: $error\n";
+   } else {
+   	print "connected to display\n";
+   }
+
+
+=head2 Checking if GUI functionality is accesiible.
+
+Without X11 connection, no GUI functionality such as screen grabbing will be
+accessible. In addition to that functionality, windowing functions will only
+become accessible after L<Prima::Application> creates a single instance
+C<$::application>.
+
+Therefore, if C<$::application> is defined, then all GUI functions can be
+safely used. If, on the contrary, it is not defined, initiate it as this:
+
+        unless ( $::application) {
+                my $error = Prima::XOpenDisplay();
+                die $error if defined $error;
+                require Prima::Application;
+                import Prima::Application;
+        }
+
+=head1 AUTHOR
+
+Dmitry Karasik, E<lt>dmitry@karasik.eu.orgE<gt>.
+
+=head1 SEE ALSO
+
+L<Prima::X11>
+
+=cut
