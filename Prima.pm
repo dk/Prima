d4353c25288422701c3f656c4736b7b6e8c1ac84
Dmitry Karasik <dmitry@karasik.eu.org>
2011-07-25 19:35:54 +0200

    - v1.31

--- Prima.pm
+++ Prima.pm
@@ -34,7 +34,7 @@ require DynaLoader;
 use vars qw($VERSION @ISA $__import @preload);
 @ISA = qw(DynaLoader);
 sub dl_load_flags { 0x00 }
-$VERSION = '1.30';
+$VERSION = '1.31';
 bootstrap Prima $VERSION;
 unless ( UNIVERSAL::can('Prima', 'init')) {
 	$::application = 0;
