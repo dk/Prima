33bd0a239834c4875e2fb0174ada711674671cdf
Dmitry Karasik <dmitry@karasik.eu.org>
2014-04-01 20:26:43 +0200

    - add rubberband example

---
+++ examples/rubberband.pl
@@ -0,0 +1,35 @@
+use strict;
+use warnings;
+use Prima qw(Application RubberBand);
+
+sub xordraw
+{
+	my ($self, @new_rect) = @_;
+	my $o = $::application;
+	$o-> begin_paint;
+	$o-> rubberband( @new_rect ?
+		( rect => \@new_rect ) :
+		( destroy => 1 )
+	);
+	$o-> end_paint;
+}
+
+Prima::MainWindow-> create(
+	onMouseDown => sub {
+		my ( $self, $btn, $mod, $x, $y) = @_;
+		$self-> {anchor} = [$self-> client_to_screen( $x, $y)];
+		xordraw( $self, @{$self-> {anchor}}, $self-> client_to_screen( $x, $y));
+		$self-> capture(1);
+	},
+	onMouseMove => sub {
+		my ( $self, $mod, $x, $y) = @_;
+		xordraw( $self, @{$self-> {anchor}}, $self-> client_to_screen( $x, $y)) if $self-> {anchor};
+	},
+	onMouseUp => sub {
+		my ( $self, $btn, $mod, $x, $y) = @_;
+		xordraw if delete $self-> {anchor};
+		$self-> capture(0);
+	},
+);
+
+run Prima;
