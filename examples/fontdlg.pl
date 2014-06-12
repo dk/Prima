9917b7861b8514e54c1ab9109c9fd60dec977eb6
Dmitry Karasik <dmitry@karasik.eu.org>
2014-06-12 12:25:46 +0200

    - show proper A-ring

--- examples/fontdlg.pl
+++ examples/fontdlg.pl
@@ -58,6 +58,7 @@ use Prima::Application name => "Font Dialog";
 use Prima::Lists;
 use Prima::Sliders;
 use Prima::Buttons;
+use Encode;
 
 # try to use perl5.8 glyph names
 eval "use charnames qw(:full);";
@@ -291,8 +292,7 @@ onClick   => sub {
 			$p-> font-> direction(0);
 
 			my $m = $p-> get_font;
-			my $xtext = ( $m-> {firstChar} < 128) ? "ÁMg" : 
-				join('', map { chr($_+$m-> {firstChar})} 51,52,0x430,0x431,0x440);
+			my $xtext = Encode::decode('latin1', "\x{c5}Mg"); 
 			my $s = $size[1] - $m-> {height} - $m-> {externalLeading} - 20;
 			my $w = $p-> get_text_width($xtext) + 66;
 			$p-> textOutBaseline(1);
