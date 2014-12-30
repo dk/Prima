f11f96e46497755228cacb28c66107c58398e5f7
David Mertens <dcmertens.perl@gmail.com>
2014-12-30 10:43:05 -0500

    Edit.pm: Add Ctrl-Delete

--- Prima/Edit.pm
+++ Prima/Edit.pm
@@ -108,6 +108,12 @@ sub profile_default
 				$_[0]-> cursor_shift_key('ShiftWordLeft');
 				$_[0]-> delete_block;
 			}],
+			[ CtrlDelete     => 0, 0, kb::Delete|km::Ctrl, sub {
+				return if $_[0]-> {readOnly};
+				return $_[0]-> delete_block if $_[0]-> has_selection;
+				$_[0]-> cursor_shift_key('ShiftWordRight');
+				$_[0]-> delete_block;
+			}],
 			[ DeleteChunk    => 0, 0, '^Y',          sub {$_[0]-> delete_current_chunk unless $_[0]-> {readOnly}}],
 			[ DeleteToEnd    => 0, 0, '^E',          sub {$_[0]-> delete_to_end unless $_[0]-> {readOnly}}],
 			[ DupLine        => 0, 0, '^K',          sub {$_[0]-> insert_line($_[0]-> cursorY, $_[0]-> get_line($_[0]-> cursorY)) unless $_[0]-> {readOnly}}],
