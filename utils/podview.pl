1b075ab692ffc255814754d41de9de8c26f12f5f
Dmitry Karasik <dmka@novozymes.com>
2016-10-10 15:56:03 +0200

    add pod in podview, show it by default

--- utils/podview.pl
+++ utils/podview.pl
@@ -18,7 +18,7 @@ package main;
 
 $Prima::HelpViewer::windowClass = 'SoleHelpViewer';
 
-my $htx = ( @ARGV ? $ARGV[0] : 'Prima' );
+my $htx = ( @ARGV ? $ARGV[0] : $0 );
 if ( -f $htx) {
 	$htx = "file://$htx";
 } else {
@@ -27,3 +27,27 @@ if ( -f $htx) {
 $::application-> open_help( $htx);
 
 run Prima;
+
+=pod
+
+=head1 NAME
+
+podview - graphical pod viewer
+
+=head2 DESCRIPTION
+
+The small GUI browser for POD format. Given the argument either
+as a file path, or perl module name (f.ex. I<File::Copy>), displays
+the documentation found.
+
+=head2 SEE ALSO
+
+L<perlpod> - the Plain Old Documentation format
+
+L<Prima> - perl graphic toolkit the viewer is based on
+
+L<Prima::HelpViewer> - menu commands explained
+
+=head1 AUTHORS
+
+Dmitry Karasik E<lt>dmitry@karasik.eu.orgE<gt>
