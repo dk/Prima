ebc57ad83366d5b97b8eb5295b86053197dc39be
Dmitry Karasik <dmka@novozymes.com>
2017-02-06 10:25:22 +0100

    minor style fix

--- Prima/Spinner.pm
+++ Prima/Spinner.pm
@@ -9,19 +9,13 @@ sub profile_default
 	my $self = shift;
 	return {
 		%{$self-> SUPER::profile_default},
-		style => 'circle',
+		style       => 'circle',
 		scaleFactor => 3,
-		active => 1,
-		buffered => 1,
-		color => cl::White, 
+		active      => 1,
+		buffered    => 1,
+		color       => cl::White, 
 		hiliteColor => cl::Black
 	}
-}
-
-sub profile_check_in
-{
-	my ( $self, $p, $default) = @_;
-	$self-> SUPER::profile_check_in( $p, $default);
 }
 
 sub init
@@ -29,11 +23,10 @@ sub init
 	my $self = shift;
 	
 	# Create the timer for the motion of the spinner
-	$self->{timer} = $self->insert(
-	'Timer',
-	name => 'Timer',
-	timeout => 200,
-	delegations => ['Tick'],
+	$self->{timer} = $self->insert( Timer => 
+		name        => 'Timer',
+		timeout     => 200,
+		delegations => ['Tick'],
 	);
 	
 	# Init the Spinner
@@ -51,19 +44,19 @@ sub init
 sub Timer_Tick {
 	my $self = shift;
 	my $startang = $self->{start_angle}; #|| 0;
-		my $endang = $self->{end_angle}; #|| 360;
-		if ($self->{style} eq 'drops') {
-			$self->{start_angle} = $startang + 1 if ($startang < 7);
-			$self->{start_angle} = 0 if ($startang == 7);
-		}
-		else {
-			$self->{start_angle} = $startang - 1 if ($startang > 0);
-			$self->{start_angle} = 360 if ($startang == 0);
-		}
-		$self->{end_angle} = $endang - 1 if ($endang > 0);
-		$self->{end_angle} = 360 if ($endang == 0);
-		
-		$self->repaint();
+	my $endang = $self->{end_angle}; #|| 360;
+	if ($self->{style} eq 'drops') {
+		$self->{start_angle} = $startang + 1 if ($startang < 7);
+		$self->{start_angle} = 0 if ($startang == 7);
+	}
+	else {
+		$self->{start_angle} = $startang - 1 if ($startang > 0);
+		$self->{start_angle} = 360 if ($startang == 0);
+	}
+	$self->{end_angle} = $endang - 1 if ($endang > 0);
+	$self->{end_angle} = 360 if ($endang == 0);
+	
+	$self->repaint();
 }
 
 # Events
@@ -72,78 +65,135 @@ sub on_paint {
 
 	$canvas->clear;
 		
-	my $x = $self->width/2; my $y = $self->height/2;
+	my $x = $self->width/2; 
+	my $y = $self->height/2;
 	my $scale_factor = $self->scaleFactor;
 	my $color1 = $self->color;
-	my $color2 = $self->hiliteColor;	
-		if ($self->{style} eq "circle") {
-			$canvas->color($color1);
-		 	$canvas->fill_ellipse($x, $y, 20*$scale_factor,20*$scale_factor);
-			$canvas->color($self->backColor);
-			$canvas->fill_ellipse($x, $y, 14*$scale_factor,14*$scale_factor);
-			$canvas->lineWidth(1.5*$scale_factor);
-			$canvas->color($color2);
-			$canvas->arc($x, $y, 17.5*$scale_factor,17.5*$scale_factor,$self->{start_angle}, $self->{end_angle});
-		}
-		else {
-			# top
-			if ($self->{start_angle} == 6  || $self->{start_angle} == 7 || $self->{start_angle} == 0) {
-				$canvas->color($color2);
-			}
-			else { $canvas->color($color1); }  
-			$canvas->fill_spline([ $x, $y+2*$scale_factor, $x-2*$scale_factor, $y+10*$scale_factor, $x, $y+12*$scale_factor, $x+2*$scale_factor, $y+10*$scale_factor, $x, $y+2*$scale_factor]);
-		
-			# top right
-			if ($self->{start_angle} == 7 || $self->{start_angle} == 0 || $self->{start_angle} == 1) {
-				$canvas->color($color2);
-			}
-			else { $canvas->color($color1); }
-			$canvas->fill_spline([ $x+1*$scale_factor, $y+1*$scale_factor,	$x+3*$scale_factor, $y+7*$scale_factor, $x+6*$scale_factor, $y+7*$scale_factor,	$x+6*$scale_factor, $y+4*$scale_factor, $x+1*$scale_factor, $y+1*$scale_factor]);
-			
-			# right
-			if ($self->{start_angle} == 0 || $self->{start_angle} == 1 || $self->{start_angle} == 2) {
-				$canvas->color($color2);
-			}
-			else { $canvas->color($color1); }
-			$canvas->fill_spline([ $x+2*$scale_factor, $y, $x+8*$scale_factor, $y+2*$scale_factor, $x+10*$scale_factor, $y, $x+8*$scale_factor, $y-2*$scale_factor, $x+2*$scale_factor, $y]);
-			
-			# right bottom
-			if ($self->{start_angle} == 1 || $self->{start_angle} == 2 || $self->{start_angle} == 3) {
-				$canvas->color($color2);
-			}
-			else { $canvas->color($color1); }
-			$canvas->fill_spline([ $x+1*$scale_factor, $y-1*$scale_factor, $x+6*$scale_factor, $y-4*$scale_factor, $x+6*$scale_factor, $y-7*$scale_factor, $x+3*$scale_factor, $y-7*$scale_factor, $x+1*$scale_factor, $y-1*$scale_factor]);
-		
-			# bottom
-			if ($self->{start_angle} == 2 || $self->{start_angle} == 3 || $self->{start_angle} == 4) {
-				$canvas->color($color2);
-			}
-			else { $canvas->color($color1); }
-			$canvas->fill_spline([ $x, $y-2*$scale_factor, $x+2*$scale_factor, $y-8*$scale_factor, $x, $y-10*$scale_factor, $x-2*$scale_factor, $y-8*$scale_factor, $x, $y-2*$scale_factor]);
-		
-			# bottom left
-			if ($self->{start_angle} == 3 || $self->{start_angle} == 4 || $self->{start_angle} == 5) {
-				$canvas->color($color2);
-			}
-			else { $canvas->color($color1); }
-			$canvas->fill_spline([ $x-1*$scale_factor, $y-1*$scale_factor, $x-6*$scale_factor, $y-4*$scale_factor, $x-6*$scale_factor, $y-7*$scale_factor, $x-3*$scale_factor, $y-7*$scale_factor, $x-1*$scale_factor, $y-1*$scale_factor]);
-			
-			# left
-			if ($self->{start_angle} == 4 || $self->{start_angle} == 5 || $self->{start_angle} == 6) {
-				$canvas->color($color2);
-			}
-			else { $canvas->color($color1); }
-			$canvas->fill_spline([ $x-2*$scale_factor, $y, $x-8*$scale_factor, $y+2*$scale_factor, $x-10*$scale_factor, $y, $x-8*$scale_factor, $y-2*$scale_factor, $x-2*$scale_factor, $y]);
-			
-			# top left
-			if ($self->{start_angle} == 5 || $self->{start_angle} == 6 || $self->{start_angle} == 7) {
-				$canvas->color($color2);
-			}
-			else { $canvas->color($color1); }
-			$canvas->fill_spline([ $x-1*$scale_factor, $y+1*$scale_factor,	$x-3*$scale_factor, $y+7*$scale_factor, $x-6*$scale_factor, $y+7*$scale_factor,	$x-6*$scale_factor, $y+4*$scale_factor, $x-1*$scale_factor, $y+1*$scale_factor]);
-		
-		}
+	my $color2 = $self->hiliteColor;
 
+	my $fill_spline = sub {
+		my @p;
+		for ( my $i = 0; $i < @_; $i += 2 ) {
+			push @p, $x + $_[$i]   * $scale_factor;
+			push @p, $y + $_[$i+1] * $scale_factor;
+		}
+		$canvas->fill_spline(\@p);
+	};
+
+	if ($self->{style} eq "circle") {
+		$canvas->color($color1);
+	 	$canvas->fill_ellipse($x, $y, 20*$scale_factor,20*$scale_factor);
+		$canvas->color($self->backColor);
+		$canvas->fill_ellipse($x, $y, 14*$scale_factor,14*$scale_factor);
+		$canvas->lineWidth(1.5*$scale_factor);
+		$canvas->color($color2);
+		$canvas->arc($x, $y, 17.5*$scale_factor,17.5*$scale_factor,$self->{start_angle}, $self->{end_angle});
+	}
+	else {
+		# top
+		if ($self->{start_angle} == 6  || $self->{start_angle} == 7 || $self->{start_angle} == 0) {
+			$canvas->color($color2);
+		}
+		else { $canvas->color($color1); }  
+		$fill_spline->(
+			0, 2,
+			-2, 10,
+			0, 12,
+			2, 10,
+			0, 2
+		);
+	
+		# top right
+		if ($self->{start_angle} == 7 || $self->{start_angle} == 0 || $self->{start_angle} == 1) {
+			$canvas->color($color2);
+		}
+		else { $canvas->color($color1); }
+		$fill_spline->( 
+			1, 1,
+			3, 7,
+			6, 7,
+			6, 4,
+			1, 1
+		);
+		
+		# right
+		if ($self->{start_angle} == 0 || $self->{start_angle} == 1 || $self->{start_angle} == 2) {
+			$canvas->color($color2);
+		}
+		else { $canvas->color($color1); }
+		$fill_spline->( 
+			2, 0,
+			8, 2,
+			10, 0,
+			8, -2,
+			2, 0
+		);
+		
+		# right bottom
+		if ($self->{start_angle} == 1 || $self->{start_angle} == 2 || $self->{start_angle} == 3) {
+			$canvas->color($color2);
+		}
+		else { $canvas->color($color1); }
+		$fill_spline->( 
+			1, -1,
+			6, -4,
+			6, -7,
+			3, -7,
+			1, -1
+		);
+	
+		# bottom
+		if ($self->{start_angle} == 2 || $self->{start_angle} == 3 || $self->{start_angle} == 4) {
+			$canvas->color($color2);
+		}
+		else { $canvas->color($color1); }
+		$fill_spline->(
+			0, -2,
+			2, -8,
+			0, -10,
+			-2, -8,
+			0, -2
+		);
+	
+		# bottom left
+		if ($self->{start_angle} == 3 || $self->{start_angle} == 4 || $self->{start_angle} == 5) {
+			$canvas->color($color2);
+		}
+		else { $canvas->color($color1); }
+		$fill_spline->(
+			-1, -1,
+			-6, -4,
+			-6, -7,
+			-3, -7,
+			-1, -1
+		);
+		
+		# left
+		if ($self->{start_angle} == 4 || $self->{start_angle} == 5 || $self->{start_angle} == 6) {
+			$canvas->color($color2);
+		}
+		else { $canvas->color($color1); }
+		$fill_spline->(
+			-2, 0,
+			-8, 2,
+			-10, 0,
+			-8, -2,
+			-2, 0
+		);
+		
+		# top left
+		if ($self->{start_angle} == 5 || $self->{start_angle} == 6 || $self->{start_angle} == 7) {
+			$canvas->color($color2);
+		}
+		else { $canvas->color($color1); }
+		$fill_spline->(
+			-1, 1,
+			-3, 7,
+			-6, 7,
+			-6, 4,
+			-1, 1
+		);
+	}
 }
 
 # properties
@@ -234,7 +284,9 @@ Prima::Spinner - Show a spinner animation
 
 =head1 DESCRIPTION
 
-Prima::Spinner provides a simple spinning animation with two designs and the opportunity to specify the colors of the spinning animation. This is useful to show an indeterminate progress of a running process.
+Prima::Spinner provides a simple spinning animation with two designs and the
+opportunity to specify the colors of the spinning animation. This is useful to
+show an indeterminate progress of a running process.
 
 =head1 USAGE
 
@@ -246,19 +298,28 @@ You can determine the following properties:
 
 =item style CHAR
 
-C<style> can be 'circle' or 'drops'. With C<'circle'> an arc moving around a circle is shown. C<'drops'> shows drops are shown that switches consecutively the color.
+C<style> can be 'circle' or 'drops'. With C<'circle'> an arc moving around a
+circle is shown. C<'drops'> shows drops are shown that switches consecutively
+the color.
 
 =item color COLOR
 
-Inherited from L<Prima::Widget>. C<color> manages the basic foreground color. Concrete for the spinner widget this means the background color of the circle or the color of the inactive drops.
+Inherited from L<Prima::Widget>. C<color> manages the basic foreground color.
+Concrete for the spinner widget this means the background color of the circle
+or the color of the inactive drops.
 
 =item hiliteColor COLOR
 
-Inherited from L<Prima::Widget>. The color used to draw alternate foreground areas with high contrast. For the spinner widget this means the color of the arc in the circle style or the color of the active drops in the drops style.
+Inherited from L<Prima::Widget>. The color used to draw alternate foreground
+areas with high contrast. For the spinner widget this means the color of the
+arc in the circle style or the color of the active drops in the drops style.
 
 =item active [BOOLEAN]
 
-If now parameter is passed, by this method you can get the active state of the spinning widget. '1' means that the spinner is running, '0' that it is stopped. With C<active(1)> you can start the spinner animation, with C<active(0)> you can stop it.
+If now parameter is passed, by this method you can get the active state of the
+spinning widget. '1' means that the spinner is running, '0' that it is stopped.
+With C<active(1)> you can start the spinner animation, with C<active(0)> you
+can stop it.
 
 =back
 
