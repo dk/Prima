346d53bef4eb5f6a21415561c376ac257cd63cef
Dmitry Karasik <dmitry@karasik.eu.org>
2017-02-26 22:14:42 +0100

    typo fix

--- Prima/Drawable/Path.pm
+++ Prima/Drawable/Path.pm
@@ -429,7 +429,7 @@ to be used for drawing or clipping shapes.
 	# draws elliptic spiral
 	my ( $d1, $dx ) = ( 0.8, 0.05 );
 	$canvas-> new_path->
-		scale(5, 10)->
+		scale(200, 100)->
 		rotate(45)->
 		arc( 0, 0, $d1 + $dx * 0, $d1 + $dx * 1, 0, 90)->
 		arc( 0, 0, $d1 + $dx * 2, $d1 + $dx * 1, 90, 180)->
