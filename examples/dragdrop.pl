d1366528b771cfdeed7169b82e86ca01d2dc5c5f
Dmitry Karasik <dmitry.karasik@teliacompany.com>
2020-02-03 13:29:48 +0100

    make drag example more interesting

--- examples/dragdrop.pl
+++ examples/dragdrop.pl
@@ -14,10 +14,6 @@ sub set_dnd_mode
 	my $self = shift;
 	my $dv = 0;
 	$dv |= &{$dnd::{$_}}() for grep { $self->bring($_)->checked } qw(Copy Move Link);
-	if ( $dv == 0 && $self->name =~ /Drag/ ) {
-		$self->Copy->checked(1);
-		$dv = dnd::Copy;
-	}
 	$self->{dnd_mode} = $dv;
 }
 
@@ -34,32 +30,34 @@ sub best_dnd_mode
 
 sub add_buttons
 {
-	my $self = shift;
+	my ($self, $mode) = @_;
 	my $fh = $self->font->height;
 	my $d  = $fh + 4;
 	my $h = $self->height - $fh - 5 - 4;
-	$self->{dnd_mode} = dnd::Copy;
+	$self->{dnd_mode} = $mode;
 	$self->insert([ SpeedButton =>
 		rect => [ 5, $h, 5 + $d, $h + $d ],
 		name => 'Copy',
 		text => 'C',
 		hint => 'Copy',
-		checkable => 1,
-		checked   => 1,
+		checked => $mode & dnd::Copy,
+		checkable   => 1,
 		onClick => sub { set_dnd_mode( $self ) },
 	], [ SpeedButton => 
 		rect => [ 5 + $d, $h, 5 + $d * 2, $h + $d ],
 		name => 'Move',
 		text => 'M',
 		hint => 'Move',
-		checkable => 1,
+		checkable   => 1,
+		checked => $mode & dnd::Move,
 		onClick => sub { set_dnd_mode( $self ) },
 	], [ SpeedButton => 
 		rect => [ 5 + $d * 2, $h, 5 + $d * 3, $h + $d ],
 		name => 'Link',
 		text => 'L',
 		hint => 'Link',
-		checkable => 1,
+		checkable   => 1,
+		checked => $mode & dnd::Link,
 		onClick => sub { set_dnd_mode( $self ) },
 	]
 	);
@@ -84,21 +82,34 @@ sub display_action
 	$w-> text($action);
 }
 
-$w->insert( Widget => 
-	origin => [10, 10],
-	size   => [100,100],
-	name   => 'DropText',
-	text   => "Drop\nText",
-	dndAware => 1,
-	onPaint => sub {
-		my $self = shift;
-		$self->clear;
-		$self->rectangle(0,0,$self->width-1,$self->height-1);
-		$self->draw_text($self->text,0,0,$self->size,dt::NewLineBreak|dt::Center|dt::VCenter);
-	},
+sub paint_text
+{
+	my $self = shift;
+	$self->clear;
+	$self->rectangle(0,0,$self->width-1,$self->height-1);
+	$self->draw_text($self->text,0,0,$self->size,dt::NewLineBreak|dt::Center|dt::VCenter);
+}
+
+sub paint_image 
+{
+	my $self = shift;
+	$self->clear;
+	$self->put_image_indirect($self->{image},
+		1, 1, 0, 0,
+		$self->width-2,$self->height-2,
+		$self->{image}->size,
+		rop::CopyPut
+	) if $self->{image};
+	$self->rectangle(0,0,$self->width-1,$self->height-1);
+	$self->draw_text("Drag\nImage",0,0,$self->size,dt::NewLineBreak|dt::Center|dt::VCenter);
+}
+
+sub drops
+{
+	my $fmt = shift;
 	onDragBegin => sub {
 		my ( $self, $clipboard, $action, $modmap, $x, $y) = @_;
-		$self->{dropable} = $clipboard->format_exists('Text') ? 1 : 0;
+		$self->{dropable} = $clipboard->has_format(ucfirst $fmt) ? 1 : 0;
 		$self->set(
 			color     => ($self->{dropable} ? cl::HiliteText : cl::DisabledText),
 			backColor => ($self->{dropable} ? cl::Hilite : cl::Disabled),
@@ -120,13 +131,49 @@ $w->insert( Widget =>
 		);
 		return unless $clipboard;
 		$ref->{allow} = 0;
-		if ($clipboard->format_exists('Text')) {
+		if ($clipboard->has_format(ucfirst $fmt)) {
 			$ref->{allow} = 1;
 			$ref->{action} = best_dnd_mode($self->{modmap}, $self->{dnd_mode} & $action); 
-			$self->text($clipboard->text);
+			if ( $fmt eq 'text') {
+				$self->text($clipboard->text());
+			} else {
+				$self->{image} = $clipboard->image;
+			}
 			$self->repaint;
 		}
 	},
+	onMouseDown => sub {
+		my $self = shift;
+		return if $self->{grab} or not $self->{dnd_mode};
+		return if $fmt eq 'image' and not $self->{image};
+		$self->{grab}++;
+		my $action = $self->begin_drag(
+			( $fmt eq 'text') ?
+				(text => $self->text ) :
+				(image => $self->{image}),
+			actions => $self->{dnd_mode},
+		);
+		display_action($action);
+		if ($action == dnd::Move) {
+			if ( $fmt eq 'text') {
+				$self->text('bupkis');
+			} else {
+				$self->{image} = undef;
+			}
+			$self->repaint;
+		}
+		$self->{grab}--;
+	},
+	onPaint => (( $fmt eq 'text') ? \&paint_text : \&paint_image ),
+}
+
+$w->insert( Widget => 
+	origin => [10, 10],
+	size   => [100,100],
+	name   => 'DropText',
+	text   => "Drop\nText",
+	dndAware => 1,
+	drops('text'),
 );
 
 $w->insert( Widget => 
@@ -134,109 +181,31 @@ $w->insert( Widget =>
 	size   => [100,100],
 	dndAware => 1,
 	name   => 'DropImage',
-	onPaint => sub {
-		my $self = shift;
-		$self->clear;
-		$self->put_image_indirect($self->{image},
-			1, 1, 0, 0,
-			$self->width-2,$self->height-2,
-			$self->{image}->size,
-			$self->{drag} ? rop::NotPut : rop::CopyPut
-		) if $self->{image};
-		$self->rectangle(0,0,$self->width-1,$self->height-1);
-		$self->draw_text("Drop\nImage",0,0,$self->size,dt::NewLineBreak|dt::Center|dt::VCenter);
-	},
-	onDragBegin => sub {
-		my ( $self, $clipboard, $action, $modmap, $x, $y) = @_;
-		$self->{drag} = 1;
-		$self->{dropable} = $clipboard->format_exists('Image') ? 1 : 0;
-		$self->set(
-			color     => ($self->{dropable} ? cl::HiliteText : cl::DisabledText),
-			backColor => ($self->{dropable} ? cl::Hilite : cl::Disabled),
-		);
-	},
-	onDragOver => sub {
-		my ( $self, $clipboard, $action, $modmap, $x, $y, $ref) = @_;
-		$ref->{allow} = $self->{dropable} && $self->{dnd_mode} != dnd::None;
-		$self->{modmap} = $modmap;
-		$ref->{action} = best_dnd_mode($modmap, $self->{dnd_mode}); 
-		$ref->{pad} = [ 0, 0, $self-> size ]; # don't send it anymore
-	},
-	onDragEnd => sub {
-		my ( $self, $clipboard, $action, $modmap, $x, $y, $ref) = @_;
-		$self->{drag} = 0;
-		$self->set(
-			color     => cl::Fore,
-			backColor => cl::Back,
-		);
-		return unless $clipboard;
-		$ref->{allow} = 0;
-		if ( $self->{image} = $clipboard->image) {
-			$ref->{allow} = 1;
-			$ref->{action} = best_dnd_mode($self->{modmap}, $self->{dnd_mode}); 
-			$self->repaint;
-		}
-	},
+	drops('image'),
 );
 
 $w->insert( Widget => 
 	origin => [10, 130],
 	size   => [100,100],
 	name   => 'DragText',
-	onPaint => sub {
-		my $self = shift;
-		$self->clear;
-		$self->rectangle(0,0,$self->width-1,$self->height-1);
-		$self->draw_text("Drag\nText",0,0,$self->size,dt::NewLineBreak|dt::Center|dt::VCenter);
-	},
-	onMouseDown => sub {
-		my $self = shift;
-		return if $self->{grab};
-		$self->{grab}++;
-		my $action = $self->begin_drag(
-			text    => $self->text,
-			actions => $self->{dnd_mode},
-		);
-		display_action($action);
-		$self->{grab}--;
-	},
+	dndAware => 1,
+	drops('text'),
 );
 
 $w->insert( Widget => 
 	origin => [130, 130],
 	size   => [100,100],
 	name   => 'DragImage',
-	enabled => 0,
+	dndAware => 1,
 	onCreate => sub {
 		my $self = shift;
 		$self->{image} = Prima::Image->load("$Bin/Hand.gif") or return;
 		$self->enabled(1);
 	},
-	onPaint => sub {
-		my $self = shift;
-		$self->clear;
-		$self->put_image_indirect($self->{image},
-			1, 1, 0, 0,
-			$self->width-2,$self->height-2,
-			$self->{image}->size,
-			rop::CopyPut
-		) if $self->{image};
-		$self->rectangle(0,0,$self->width-1,$self->height-1);
-		$self->draw_text("Drag\nImage",0,0,$self->size,dt::NewLineBreak|dt::Center|dt::VCenter);
-	},
-	onMouseDown => sub {
-		my $self = shift;
-		return if $self->{grab} or not $self->{image};
-		$self->{grab}++;
-		my $action = $self->begin_drag(
-			image   => $self->{image},
-			actions => $self->{dnd_mode},
-		);
-		display_action($action);
-		$self->{grab}--;
-	},
+	drops('image'),
 );
 
-add_buttons($w->bring($_)) for qw(DropText DragText DropImage DragImage);
+add_buttons($w->bring($_), dnd::Copy) for qw(DropText DragText );
+add_buttons($w->bring($_), dnd::Move) for qw(DropImage DragImage);
 
 run Prima;
