78d5af3d1bf52cf73a8f033fafc91e7b1e1d3867
Dmitry Karasik <dmitry.karasik@teliacompany.com>
2021-04-21 12:59:15 +0200

    v1.61

--- Prima.pm
+++ Prima.pm
@@ -9,7 +9,7 @@ require DynaLoader;
 use vars qw($VERSION @ISA $__import @preload $pid);
 @ISA = qw(DynaLoader);
 sub dl_load_flags { 0x00 }
-$VERSION = '1.60';
+$VERSION = '1.61';
 $pid = $$;
 bootstrap Prima $VERSION;
 unless ( UNIVERSAL::can('Prima', 'init')) {
