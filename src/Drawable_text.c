6ef016babd8b46cd393414259e2c7bfda0e126e3
Dmitry Karasik <dmitry@karasik.eu.org>
2021-05-16 19:00:24 +0200

    typo fix

--- src/Drawable_text.c
+++ src/Drawable_text.c
@@ -2600,7 +2600,7 @@ string_glyphs_wrap( Handle self, SV * text, int width, int options, int tabInden
 SV*
 Drawable_text_wrap( Handle self, SV * text, int width, int options, int tabIndent, int from, int len, SV * glyphs)
 {
-	if ( glyphs != nilSV ) {
+	if ( SvTYPE(glyphs) != SVt_NULL ) {
 		return string_glyphs_wrap(self, text, width, options, tabIndent, from, len, glyphs);
 	} else if ( !SvROK( text )) {
 		return string_wrap(self, text, width, options, tabIndent, from, len);
