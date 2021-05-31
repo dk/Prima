bcfd7fe59872a066944bd2c6723783afd8aa443b
Dmitry Karasik <dmitry@karasik.eu.org>
2021-05-31 16:46:04 +0200

    Prima::array::new can take an arrayref

--- examples/edit.pl
+++ examples/edit.pl
@@ -16,9 +16,10 @@ use warnings;
 use Prima 'InputLine', 'Application';
 
 my $w = Prima::MainWindow->create( size => [ 700, 300]);
-
+use utf8;
 my $l = $w->insert( InputLine =>
-	text        => '0:::1234 5678 90ab cdef ghij klmn oprq stuv:1::1234 5678 90ab cdef ghij klmn oprq stuv:2::1234 5678 90ab cdef ghij klmn oprq stuv:3::1234 5678 90ab cdef ghij klmn oprq stuv:4::1234 5678 90ab cdef ghij klmn oprq stuv::End',
+#	text        => '0:::1234 5678 90ab cdef ghij klmn oprq stuv:1::1234 5678 90ab cdef ghij klmn oprq stuv:2::1234 5678 90ab cdef ghij klmn oprq stuv:3::1234 5678 90ab cdef ghij klmn oprq stuv:4::1234 5678 90ab cdef ghij klmn oprq stuv::End',
+	text        => "சீனிவாசன் கணபதி",
 	centered    => 1,
 	width       => 300,
 #  firstChar   => 10,
@@ -29,5 +30,6 @@ my $l = $w->insert( InputLine =>
 	borderWidth => 3,
 	autoSelect  => 0,
 );
+$l->select_all;
 
 run Prima;
