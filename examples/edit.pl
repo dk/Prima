6bc078865e6d7db9e2a57f94a15d89d580a5d4dd
Dmitry Karasik <dmitry@karasik.eu.org>
2021-05-31 16:47:17 +0200

    typo fix

--- examples/edit.pl
+++ examples/edit.pl
@@ -16,10 +16,9 @@ use warnings;
 use Prima 'InputLine', 'Application';
 
 my $w = Prima::MainWindow->create( size => [ 700, 300]);
-use utf8;
+
 my $l = $w->insert( InputLine =>
-#	text        => '0:::1234 5678 90ab cdef ghij klmn oprq stuv:1::1234 5678 90ab cdef ghij klmn oprq stuv:2::1234 5678 90ab cdef ghij klmn oprq stuv:3::1234 5678 90ab cdef ghij klmn oprq stuv:4::1234 5678 90ab cdef ghij klmn oprq stuv::End',
-	text        => "சீனிவாசன் கணபதி",
+	text        => '0:::1234 5678 90ab cdef ghij klmn oprq stuv:1::1234 5678 90ab cdef ghij klmn oprq stuv:2::1234 5678 90ab cdef ghij klmn oprq stuv:3::1234 5678 90ab cdef ghij klmn oprq stuv:4::1234 5678 90ab cdef ghij klmn oprq stuv::End',
 	centered    => 1,
 	width       => 300,
 #  firstChar   => 10,
@@ -30,6 +29,5 @@ my $l = $w->insert( InputLine =>
 	borderWidth => 3,
 	autoSelect  => 0,
 );
-$l->select_all;
 
 run Prima;
