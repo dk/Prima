ee221b4e43047a46b88314c99ec0bf033e055d59
Dmitry Karasik <dmitry.karasik@teliacompany.com>
2022-02-21 22:13:53 +0100

    add ScrollGroup demo

---
+++ examples/scrollgroup.pl
@@ -0,0 +1,27 @@
+=pod
+
+=head1 NAME
+
+examples/scrollwidget.pl - scrolling dialog panel
+
+=head1 FEATURES
+
+Panel with widgets that is too big for the screen
+
+=cut
+
+use strict;
+use warnings;
+use Prima qw(ScrollWidget InputLine Application);
+
+my $w = Prima::MainWindow->new( packPropagate => 0);
+
+my $sc = $w->insert( ScrollGroup =>
+	pack => { expand => 1, fill => 'both' },
+);
+$sc->insert( InputLine => pack => { side => 'bottom' }) for 1..20;
+$w->packPropagate(1);
+$w->height( $w->height / 2);
+
+
+run Prima;
