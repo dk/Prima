ab48807dda0dede496056e7e8c56df529328a0da
Dmitry Karasik <dmitry@karasik.eu.org>
2022-11-19 23:47:14 +0100

    msvc fix

--- include/apricot.h
+++ include/apricot.h
@@ -4069,6 +4069,9 @@ apc_fs_opendir( const char *path, PDirHandleRec dh);
 extern int
 apc_fs_open_file( const char* path, Bool is_utf8, int flags, int mode);
 
+#ifndef PATH_MAX
+#	define PATH_MAX 2047
+#endif
 #define PATH_MAX_UTF8 (PATH_MAX*6)
 extern Bool
 apc_fs_readdir( PDirHandleRec dh, char * entry);
