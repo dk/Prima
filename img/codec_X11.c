c24c02ff0c99df7bcde83ffa6106b599ec195893
Dmitry Karasik <dmitry@karasik.eu.org>
2024-05-13 20:23:45 +0200

    retab

--- img/codec_X11.c
+++ img/codec_X11.c
@@ -205,13 +205,13 @@ open_save( PImgCodec instance, PImgSaveFileInstance fi)
 static void
 myprintf( PImgIORequest req, const char *format, ...)
 {
-		int len;
-		char buf[2048];
-		va_list args;
-		va_start( args, format);
-		len = vsnprintf( buf, 2048, format, args);
-		va_end( args);
-		req_write( req, len, buf);
+	int len;
+	char buf[2048];
+	va_list args;
+	va_start( args, format);
+	len = vsnprintf( buf, 2048, format, args);
+	va_end( args);
+	req_write( req, len, buf);
 }
 
 static Bool
