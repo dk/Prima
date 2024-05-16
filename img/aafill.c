495a3ede0d95cc051a09049805941b8fdf342f4d
Dmitry Karasik <dmitry.karasik@teliacompany.com>
2024-05-16 17:59:45 +0200

    split polyfill code

---
+++ img/aafill.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "img_conv.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+Bool
+img_aafill( Handle self, NPoint *pts, int n_pts, int rule, PImgPaintContext ctx)
+{
+	return false;
+}
+
+#ifdef __cplusplus
+}
+#endif
