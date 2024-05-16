865c5f94644b6af665414078d3de285b1b92d3f1
Dmitry Karasik <dmitry.karasik@teliacompany.com>
2024-05-16 23:18:12 +0200

    update for today

--- include/img_conv.h
+++ include/img_conv.h
@@ -375,22 +375,14 @@ extern PRegionRec img_region_polygon( Point *pts, int count, int rule);
 extern PRegionRec img_region_mask( Handle mask);
 extern void img_region_offset( PRegionRec region, int dx, int dy);
 
-/* polygon renderer */
-
-/*
- * number of points to buffer before sending them off
- * to scanlines() :  Must be an even number
- */
-#undef NUMPTSTOBUFFER
-#define NUMPTSTOBUFFER 200
 /*
  * used to allocate buffers for points and link
  * the buffers together
  */
 typedef struct _PolyPointBlock {
-	Point pts[NUMPTSTOBUFFER];
 	struct _PolyPointBlock *next;
 	unsigned int size;
+	Point pts[1];
 } PolyPointBlock;
 
 extern PolyPointBlock* poly_poly2points(Point *pts, int count, int rule);
