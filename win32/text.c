b5bad897adf59a1aa6db4f906a2a0430c98583f6
Dmitry Karasik <dmitry@karasik.eu.org>
2024-10-03 19:57:36 +0200

    fix resource leak

--- win32/text.c
+++ win32/text.c
@@ -2028,12 +2028,14 @@ apc_font_begin_query( Handle self )
 {
 	if ( !( sys ps = CreateCompatibleDC( NULL )))
 		return false;
+	sys stock_font = GetCurrentObject( sys ps, OBJ_FONT);
 	return true;
 }
 
 void
 apc_font_end_query( Handle self )
 {
+	SelectObject( sys ps, sys stock_font);
 	DeleteDC( sys ps );
 	sys ps = 0;
 }
