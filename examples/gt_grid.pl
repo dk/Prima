86a9a409824583592d8d905f8b462d5c9cbc1584
Dmitry Karasik <dmitry.karasik@teliacompany.com>
2025-02-19 18:56:16 +0100

    retab

--- examples/gt_grid.pl
+++ examples/gt_grid.pl
@@ -17,9 +17,9 @@ use Prima qw( Application Label InputLine Sliders );
 use Prima::Drawable::Markup;
 
 my $w = Prima::MainWindow-> new(
-    text      => 'Grid geometry manager example',
-    backColor => cl::Gray,
-    visible => 0,
+	text      => 'Grid geometry manager example',
+	backColor => cl::Gray,
+	visible => 0,
 );
 
 my ($ipadx,$ipady,$padx,$pady) = (0,0,1,1);
@@ -31,75 +31,75 @@ END
 $text = Prima::Drawable::Markup->new(markup => $text);
 
 my @rows = (
-    [
-        $w->insert(
-            Label =>
-            text => \"The Prima Grid Geometry Manager",
-            backColor => hex('C0C0C0'),
-        )
-    ],
-    [
-        $w->insert(
-            Label =>
-            text => 'ipadx',
-            backColor => cl::White,
-        ),
-        $w->insert(
-            InputLine =>
-            text => $ipadx,
-            onChange => sub { set_ipadx($_[0]->text || 0) },
-            backColor => cl::White,
-        ),
-    ],
-    [
-        $w->insert(
-            Label =>
-            text => 'ipady',
-            backColor => cl::White,
-        ),
-        $w->insert(
-            CircularSlider =>
-            min => 0, max => 10,
-            autoTrack => 0,
-            onChange => sub { set_ipady($_[0]->value) },
-            backColor => cl::White,
-        ),
-    ],
-    [
-        $w->insert(
-            Label =>
-            text => 'padx',
-            backColor => cl::White,
-        ),
-        $w->insert(
-            Slider =>
-            min => 0, max => 10,
-            value => 1,
-            onChange => sub { set_padx($_[0]->value) },
-            backColor => cl::White,
-        ),
-    ],
-    [
-        $w->insert(
-            Label =>
-            text => 'pady',
-            backColor => cl::White,
-        ),
-        $w->insert(
-            InputLine =>
-            text => $pady,
-            onChange => sub { set_pady($_[0]->text || 0) },
-            backColor => cl::White,
-        ),
-        $w->insert(
-            Label =>
-            text => $text,
-            backColor => cl::White,
-            autoHeight => 1,
-            wordWrap => 1,
-            width => 180,
-        ),
-    ],
+	[
+		$w->insert(
+			Label =>
+			text => "The Prima Grid Geometry Manager",
+			backColor => 0xC0C0C0,
+		)
+	],
+	[
+		$w->insert(
+			Label =>
+			text => 'ipadx',
+			backColor => cl::White,
+		),
+		$w->insert(
+			InputLine =>
+			text => $ipadx,
+			onChange => sub { set_ipadx($_[0]->text || 0) },
+			backColor => cl::White,
+		),
+	],
+	[
+		$w->insert(
+			Label =>
+			text => 'ipady',
+			backColor => cl::White,
+		),
+		$w->insert(
+			CircularSlider =>
+			min => 0, max => 10,
+			autoTrack => 0,
+			onChange => sub { set_ipady($_[0]->value) },
+			backColor => cl::White,
+		),
+	],
+	[
+		$w->insert(
+			Label =>
+			text => 'padx',
+			backColor => cl::White,
+		),
+		$w->insert(
+			Slider =>
+			min => 0, max => 10,
+			value => 1,
+			onChange => sub { set_padx($_[0]->value) },
+			backColor => cl::White,
+		),
+	],
+	[
+		$w->insert(
+			Label =>
+			text => 'pady',
+			backColor => cl::White,
+		),
+		$w->insert(
+			InputLine =>
+			text => $pady,
+			onChange => sub { set_pady($_[0]->text || 0) },
+			backColor => cl::White,
+		),
+		$w->insert(
+			Label =>
+			text => $text,
+			backColor => cl::White,
+			autoHeight => 1,
+			wordWrap => 1,
+			width => 180,
+		),
+	],
 );
 
 $w->gridConfigure($rows[0][0], row => 4, column => 0, colspan => 3, sticky => 'we',);
@@ -120,16 +120,16 @@ sub set_padx  { $padx  = shift; reconfigure() };
 sub set_pady  { $pady  = shift; reconfigure() };
 
 sub reconfigure {
-    $w->gridLock;
-    for my $row (@rows) {
-        for my $cell (@$row) {
-            $cell->gridConfigure(
-                ipadx => $ipadx,  ipady => $ipady,
-                padx  => $padx,   pady  => $pady,
-            );
-        }
-    }
-    $w->gridUnlock;
+	$w->gridLock;
+	for my $row (@rows) {
+		for my $cell (@$row) {
+			$cell->gridConfigure(
+				ipadx => $ipadx,  ipady => $ipady,
+				padx  => $padx,   pady  => $pady,
+			);
+		}
+	}
+	$w->gridUnlock;
 }
 
 $w->show;
